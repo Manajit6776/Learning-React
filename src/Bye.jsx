fe9d44512ff69f91ec6b35734bc5971af294b1de
Manajit6776 <manajit.mondal20@gmail.com>
2026-04-25 21:31:22 +0530

    Create 2 new .jsx file and learn the import export

---
+++ src/Bye.jsx
@@ -0,0 +1,9 @@
+function Bye(){
+    return(
+        <>
+            <h1>Bye Visit Again!</h1>
+        </>
+    )
+}
+
+export default Bye
---
+++ src/Hello.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function Hello(){
+    const name = "Manajit";
+ return(
+    <>
+        <h1 className="">Hello {name}</h1>
+    </>
+ )
+}
+
+export default Hello
